615361523a1c34cc58abd4e56d7fbb9e4c8ea5ce
Naxthephantom <80404140+Naxthephantom@users.noreply.github.com>
2022-03-12 15:49:21 +0100

    Update Server.py

--- Server.py
+++ Server.py
@@ -2,3 +2,12 @@ import qrcode               #QR code library
 img = qrcode.make()
 type(img)
 img.save()
+
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class myserver(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        self.send_response(200)
