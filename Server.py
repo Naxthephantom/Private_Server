0ae3d8d9feeb9720baf11901a2b6a72271a1bb0d
Naxthephantom <80404140+Naxthephantom@users.noreply.github.com>
2022-03-13 16:04:54 +0100

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,13 +1,25 @@
-import qrcode               #QR code library  
-img = qrcode.make()
-type(img)
-img.save()
-
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
+hostname = 'localhost'
+port = 2030
 class myserver(BaseHTTPRequestHandler):
     
     def do_GET(self):
-        self.send_response(200)
+        if self.path == '/':
+            self.path = '/'
+        try:
+            open_file = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            open_file = "File can not be located"
+            self.send_response(404)
+        self.end_headers
+        self.wfile.write(bytes(open_file, 'utf-8'))
+
+
+
+pserver = HTTPServer((hostname, port), myserver)
+pserver.serve_forever
+
